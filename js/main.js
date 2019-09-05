5c9bf91e39519d1905f3282a8c315543531119d6
CoolSpam <54927479+CoolSpam@users.noreply.github.com>
2019-09-05 15:58:12 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    items: 5,
+    loop:true,
+    margin:20,
+    nav: true,
+    // navText : ['next','prev']
+
+    
+	})
+});
+
